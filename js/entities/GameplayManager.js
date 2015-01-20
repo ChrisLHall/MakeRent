5ab75ad3a208956ffbec629803a1078aa00e911e
Lucas Karahadian <lucask4@gmail.com>
2015-01-20 15:59:27 -0800

    Random pathing of world
    The scrolling of the world now moves around at random as opposed to
    just moving right.  However, this means that the code for the scrolling
    background needs to be redone.  Get ‘er done Chris.

--- js/entities/GameplayManager.js
+++ js/entities/GameplayManager.js
@@ -21,6 +21,7 @@ game.GameplayManager = me.Entity.extend({
         // jk this absolutely does not work...ok now maybe?
         this.SCROLL_SPEED = 0.6;
         this.transition = false;
+        this.direction = "right";
         this.spawning = true;
         this.enemyCount = 0;
         this.spawnedEnemies = 0;
@@ -32,33 +33,55 @@ game.GameplayManager = me.Entity.extend({
 
 
     spawn: function() {
-        var y1 = Math.floor(Math.random() * 15);
-        var y2 = Math.floor(Math.random() * 15);
-        while (y2 == y1) {
-            y2 = Math.floor(Math.random() * 15);
-        }
-        var y3 = Math.floor(Math.random() * 15);
-        while (y3 == y1 || y3 == y2) {
-            y3 = Math.floor(Math.random() * 15);
-        }
-        var y4 = Math.floor(Math.random() * 15);
-        while (y4 == y1 || y4 == y2 || y4 == y3) {
-            y4 = Math.floor(Math.random() * 15);
+        var r1 = Math.floor(Math.random() * 15);
+        var r2 = Math.floor(Math.random() * 15);
+        while (r2 == r1) {
+            r2 = Math.floor(Math.random() * 15);
         }
 
-        var x = me.game.viewport.right;
+        switch (this.direction) {
+            case "right":
+                var x1 = x2 = me.game.viewport.right;
+                var y1 = r1 * 32 + me.game.viewport.top;
+                var y2 = r2 * 32 + me.game.viewport.top;
+                var newvelx = 2;
+                var newvely = 0;
+                break;
+            case "left":
+                var x1 = x2 = me.game.viewport.left;
+                var y1 = r1 * 32 + me.game.viewport.top;
+                var y2 = r2 * 32 + me.game.viewport.top;
+                var newvelx = -2;
+                var newvely = 0;
+                break;
+            case "up":
+                var x1 = r1 * 43 + me.game.viewport.left;
+                var x2 = r2 * 43 + me.game.viewport.left;
+                var y1 = y2 = me.game.viewport.top;
+                var newvelx = 0;
+                var newvely = -2;
+                break;
+            case "down":
+                var x1 = r1 * 43 + me.game.viewport.left;
+                var x2 = r2 * 43 + me.game.viewport.left;
+                var y1 = y2 = me.game.viewport.bottom;
+                var newvelx = 0;
+                var newvely = 2;
+                break;
+        };
         if (this.transition) {
-            me.game.world.addChild(new game.ObstacleEntity(x, y1 * 32));
-            me.game.world.addChild(new game.ObstacleEntity(x, y2 * 32));
+            me.game.world.addChild(new game.ObstacleEntity(x1, y1));
+            me.game.world.addChild(new game.ObstacleEntity(x2, y2));
         } else {
             if (this.spawnedEnemies < 6 && this.spawning) {
-                me.game.world.addChild(new game.EnemyEntity(x, y3 * 32));
+                me.game.world.addChild(new game.EnemyEntity(x1, y1));
             } else {
                 this.spawning = false;
             }
             if (this.enemyCount < 1) {
                 this.transition = true;
-                this.body.vel.x = 2;
+                this.body.vel.y = newvely;
+                this.body.vel.x = newvelx;
                 me.timer.setTimeout(this.stopTransition.bind(this), 6000);
             }
         }
@@ -72,6 +95,21 @@ game.GameplayManager = me.Entity.extend({
         this.spawning = true;
         this.spawnedEnemies = 0;
         this.body.vel.x = 0;
+        this.body.vel.y = 0;
+        switch (Math.floor(Math.random() * 4)) {
+            case 0:
+                this.direction = "right";
+                break;
+            case 1:
+                this.direction = "left";
+                break;
+            case 2:
+                this.direction = "up";
+                break;
+            case 3:
+                this.direction = "down";
+                break;
+        };
     },
 
     /** Update the block's animation frame based on mood. Destroy it if it is
